0f5cf915c0af580115478a4a1e3fe7a20a9647ee
Cristian Cortez <cortez.cristian@gmail.com>
2016-04-14 00:55:16 -0300

    Cyclic Rotation

--- cyclicrotation/src/index.js
+++ cyclicrotation/src/index.js
@@ -1,21 +1,15 @@
 var solution = function (A, K) {
     var B = [];
-    if(A.length === 0) {
-				return A;
-    } if(K < A.length) {
-        B = A.splice((K==1)?1:K-1,A.length+1)
-        console.log("Menor A>", A, "B>", B);
-        return B.concat(A);
-    } else if (K ==  A.length)  {
-        //B = A.splice(0, K);
+		if(A.length === 0 || A.length === K) {
         return A;
+    } else if (K < A.length) {
+        B = A.splice((A.length-1-K+1), K);
+        return B.concat(A);
     } else {
         do{
             K -= A.length;
         }while(K > A.length)
-        //B = A.splice(0, K-1);
-        B = A.splice((K==1)?1:K,A.length+1)
-        console.log("Mayor A>", A, "B>", B);
+        B = A.splice((A.length-1-K+1), K);
         return B.concat(A);
     }
 }

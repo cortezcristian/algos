100232a2c14dc8fa998be6dfcac74ebb883c03f7
Cristian Cortez <cortez.cristian@gmail.com>
2016-04-14 01:48:24 -0300

    Bitwise operator

--- oddoccurrencesinarray/src/index.js
+++ oddoccurrencesinarray/src/index.js
@@ -1,22 +1,13 @@
 var solution = function (A) {
-		// Detected time complexity: O(N**2)
-    if(A.length % 2 === 0) {
-        return;
-    } else {
-        do {
-            var a = A.pop();
-            var x = A.length;
-            for (var i = 0; i < A.length; i++){
-                if(a === A[i]){
-                    A.splice(i,1);
-                }
-            }
-            if(A.length === x){
-                A = [a];
-            }
-        } while (A.length > 1);
-        return A[0];
+		// Detected time complexity: O(N) or O(N*log(N))
+		// Based on
+		// https://codesays.com/2015/solution-to-odd-occurrences-in-array-by-codility/
+		// https://codesays.com/2014/solution-to-perm-missing-elem-by-codility/
+		var res = 0, num;
+    for(num in A){
+        res = res ^ A[num];
     }
+    return res;
 }
 
 module.exports = solution;

4e1949545cddfbc0056b230ad45f1276e3ab4527
Cristian Cortez <cortez.cristian@gmail.com>
2016-04-16 21:26:52 -0300

    New solution

--- tapeEquilibrium/src/index.js
+++ tapeEquilibrium/src/index.js
@@ -1,17 +1,20 @@
 var solution = function (A) {
 		// http://stackoverflow.com/a/19667905/467034
-    var min = null, total = 0, acum = 0;
-    total = A.reduce(function(a,b){return a+b;});
-
-    for(var i = 0; i < A.length ; i++) {
-        total -= A[i];
-        var res = Math.abs( (acum + A[i]) - total );
-        //console.log(res);
+		// Detected time complexity: O(N)
+    var right = A.reduce(function(a,b){ return a+b;});
+    var left = 0;
+    var min = null;
+    //console.log(">>", right);
+    for(var i=0;i<A.length;i++){
+        left += A[i];
+        right -= A[i];
+        var res = Math.abs(left - right);
+        console.log("left: ", left, "right:", right, "res: ", res);
         if(min === null || res < min) {
             min = res;
+            console.log("new min: ", min);
         }
     }
-
     return min;
 }
 
--- tapeEquilibrium/test/test.js
+++ tapeEquilibrium/test/test.js
@@ -2,8 +2,8 @@ var assert = require("assert");
 var TapeEquilibrium = require("../src/index.js")
 
 describe("Tape Equilibrium Tests", function(){
-  it("Test TapeEquilibrium([3, 1, 2, 4, 3], 3) => 7", function(done){
-    assert.deepEqual(TapeEquilibrium([3, 1, 2, 4, 3]), 7);
+  it("Test TapeEquilibrium([3, 1, 2, 4, 3], 3) => 1", function(done){
+    assert.deepEqual(TapeEquilibrium([3, 1, 2, 4, 3]), 1);
     done();
   });
 
